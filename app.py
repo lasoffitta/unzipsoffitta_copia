17ed93a5d7cf7f06823a15bc3fc7ed9054d84aa2
francoross <76166342+francoross@users.noreply.github.com>
2024-03-23 13:14:07 +0100

    webhook_update

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Koyeb'
 
+@app.route('/telegram', methods=['POST'])
+def handle_telegram_webhook():
+    data = json.loads(request.data)  # Carica i dati del messaggio
+    # Gestisci il messaggio qui
+    return 'ok'
 
 if __name__ == "__main__":
     app.run()
