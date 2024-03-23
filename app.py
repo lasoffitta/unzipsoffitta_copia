e08480265d1b7c294e3ac5b4d4f27cbe52397b0d
francoross <76166342+francoross@users.noreply.github.com>
2024-03-23 21:58:24 +0100

    gestione estrazione

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
-from telegram import Bot, Update
+from telegram import Bot, Update, InputFile
 from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
 import os
+import zipfile
+import rarfile
 
 app = Flask(__name__)
 
@@ -21,11 +23,28 @@ def start(update, context):
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+def handle_document(update, context):
+    file = context.bot.get_file(update.message.document.file_id)
+    file.download('file.rar')
+
+    if rarfile.is_rarfile('file.rar'):
+        with rarfile.RarFile('file.rar') as rf:
+            rf.extractall()
+    elif zipfile.is_zipfile('file.rar'):
+        with zipfile.ZipFile('file.rar') as zf:
+            zf.extractall()
+
+    for filename in os.listdir('.'):
+        with open(filename, 'rb') as f:
+            context.bot.send_document(chat_id='6925431313', document=InputFile(f))
+
 start_handler = CommandHandler('start', start)
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+document_handler = MessageHandler(Filters.document, handle_document)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(document_handler)
 
 if __name__ == '__main__':
     app.run(port=8000)
