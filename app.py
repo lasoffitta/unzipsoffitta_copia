e46d9482f82fd4755ca5a53ec574d3057bad140f
francoross <76166342+francoross@users.noreply.github.com>
2024-03-23 20:12:57 +0100

    Update app.py aggiunto flask

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request
 from telethon import TelegramClient, events
+from telegram import Bot
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 
 app = Flask(__name__)
 
@@ -8,25 +10,28 @@ api_id = os.environ['TELEGRAM_API_ID']
 api_hash = os.environ['TELEGRAM_API_HASH']
 bot_token = os.environ['TELEGRAM_BOT_TOKEN']
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+telethon_bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+telegram_bot = Bot(bot_token)
+
+updater = Updater(bot=telegram_bot, use_context=True)
 
 @app.route('/')
 def home():
     return "Hello, World!"
 
-@bot.on(events.NewMessage(pattern='/start'))
+@telethon_bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
     await event.respond('Ciao! Sono il tuo bot. Per favore, invia o carica un file .zip o .rar.')
     raise events.StopPropagation
 
-@bot.on(events.NewMessage)
+@telethon_bot.on(events.NewMessage)
 async def echo(event):
     await event.respond(event.text)
 
 def main():
-    bot.start()
+    telethon_bot.start()
     app.run()
-    bot.run_until_disconnected()
+    telethon_bot.run_until_disconnected()
 
 if __name__ == '__main__':
     main()
