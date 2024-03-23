93295d0d25dfa2d75ba5fb9ba3fde4d19407c141
francoross <76166342+francoross@users.noreply.github.com>
2024-03-23 19:19:34 +0100

    Update app.py ripristinati moduli

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
+from flask import Flask, request
 from telethon import TelegramClient, events
+
+app = Flask(__name__)
 
 api_id = os.environ['TELEGRAM_API_ID']
 api_hash = os.environ['TELEGRAM_API_HASH']
 bot_token = os.environ['TELEGRAM_BOT_TOKEN']
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
 
 @bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
@@ -17,6 +24,8 @@ async def echo(event):
     await event.respond(event.text)
 
 def main():
+    bot.start()
+    app.run()
     bot.run_until_disconnected()
 
 if __name__ == '__main__':
